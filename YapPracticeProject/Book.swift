50cf86439e476fa5ff45da48f29faa7ffcae02ae
Mark Koslow <markkoslow@Marks-MacBook-Pro-2.local>
2016-04-07 10:34:40 -0400

    Add isbn number to books object.

--- YapPracticeProject/Book.swift
+++ YapPracticeProject/Book.swift
@@ -11,25 +11,31 @@ import Foundation
 class Book: NSObject, NSCoding {
     let title: String
     let author: String
+    let isbn: String
     
-    init(title: String, author: String) {
+    init(title: String, author: String, isbn: String) {
         self.title = title
         self.author = author
+        self.isbn = isbn
     }
     
     required init?(coder aDecoder: NSCoder) {
         if let title = aDecoder.decodeObjectForKey("title") as? String,
-            author = aDecoder.decodeObjectForKey("author") as? String {
+            author = aDecoder.decodeObjectForKey("author") as? String,
+            isbn = aDecoder.decodeObjectForKey("isbn") as? String {
             self.title = title
             self.author = author
+            self.isbn = isbn
         } else {
             self.title = ""
             self.author = ""
+            self.isbn = ""
         }
     }
     
     func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(title, forKey: "title")
         aCoder.encodeObject(author, forKey: "author")
+        aCoder.encodeObject(isbn, forKey: "isbn")
     }
 }
