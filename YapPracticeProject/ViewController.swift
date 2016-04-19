f30903d37de0356e5f5d486f6f56dd3f5ec7f69e
Mark Koslow <markkoslow@Marks-MacBook-Pro-2.local>
2016-04-19 11:37:00 -0400

    wip: Add mappings for books collection.

--- YapPracticeProject/ViewController.swift
+++ YapPracticeProject/ViewController.swift
@@ -12,18 +12,22 @@ import YapDatabase
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    let connection = Database.newConnection()
+    var mappings: YapDatabaseViewMappings?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // Read Books json
+        //
         let myFileURL = NSBundle.mainBundle().URLForResource("Books", withExtension: "json")!
         guard let fileData = NSData(contentsOfURL: myFileURL) else {
             fatalError("File could not be found")
         }
         
         // Parse Books json into dictionary
+        //
         var bookList = [Book]()
         do {
             let json = try NSJSONSerialization.JSONObjectWithData(fileData, options: .AllowFragments)
@@ -42,21 +46,63 @@ class ViewController: UIViewController {
             print("lolz fail")
         }
         
-        
         // Store Books in YAP
-        let connection = Database.newConnection()
-        var huckFinn: Book?
+        //
         connection.readWriteWithBlock({ transaction in
             for book in bookList {
                 transaction.setObject(book, forKey: book.isbn, inCollection: "Books")
             }
         })
-
+//        connection.readWithBlock { transaction in
+//            huckFinn = transaction.objectForKey("0486280616", inCollection: "Books") as? Book
+//        }
+        
+        // TO DO: Need to create Mappings
+        //
         connection.readWithBlock { transaction in
-            huckFinn = transaction.objectForKey("0486280616", inCollection: "Books") as? Book
+            let grouping = YapDatabaseViewMappingGroupFilter {
+                return { (group, transaction) -> Bool in
+                    return true
+                }
+            }
+            
+            let sorting = YapDatabaseViewMappingGroupSort {
+                return { (group1, group2, transaction) -> NSComparisonResult in
+                    return group1.caseInsensitiveCompare(group2)
+                }
+            }
+            
+            mappings = YapDatabaseViewMappings(groupFilterBlock: grouping, sortBlock: sorting, view: CustomDatabaseExtension.BookList.name)
         }
         
-        print("\(huckFinn)")
+        // Initialize mappings
+        //
+//        connection.beginLongLivedReadTransaction()
+        connection.readWithBlock { self.mappings?.updateWithTransaction($0) }
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(yapDatabaseModified:), name: YapDatabaseModifiedNotification, object: connection.database)
+        
+        
+        // Retreive objects from view
+        var count = 0
+        connection.readWithBlock { transaction in
+            
+            let myExt = transaction.ext(CustomDatabaseExtension.BookList.name)
+            count = myExt.numberOfItemsInAllGroups()
+        }
+        print(count)
+        
+        // TO DO: Propogate view with mappings
+        //
+        
+    }
+    
+    func yapDatabaseModified(notification: NSNotification) {
+        var notifications = connection.beginLongLivedReadTransaction()
+        
+        var sectionChanges = []
+        var rowChanges = []
+        
+        connection.ext(CustomDatabaseExtension.BookList.name).getSectionChanges(&sectionChanges, rowChanges: &rowChanges, forNotifications: notifications, withMappings: self.mappings)
     }
 
     override func didReceiveMemoryWarning() {
