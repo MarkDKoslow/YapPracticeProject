2e2887c16cd77f0d2b4694a594d7b41c67b7bca9
Mark Koslow <markkoslow@Marks-MacBook-Pro-2.local>
2016-04-19 11:34:15 -0400

    Finish BookList Database extension and add comments.

--- YapPracticeProject/DatabaseInitialization.swift
+++ YapPracticeProject/DatabaseInitialization.swift
@@ -12,26 +12,31 @@ import YapDatabase
 enum CustomDatabaseExtension {
     case BookList
     
-    var ext: YapDatabaseExtension {
+    var sampleExt: YapDatabaseExtension {
         switch self {
         case .BookList:
+            // This logic will be run on the entire database when the view is first created
+            // Afterwards, it will be run each time an object in the database is modified
 
+            // Grouping block determines which objects should be included in the view
             let grouping = YapDatabaseViewGrouping.withObjectBlock { (_, _, _, object) -> String! in
                 guard let book = object as? Book else { return nil }
                 return String(book.title.characters.first)
             }
             
+            // Sorting block determines how objects should be ordered in the view
             let sorting = YapDatabaseViewSorting.withObjectBlock { (_, _, _, _, object1, _, _, object2) -> NSComparisonResult in
                 guard let book1 = object1 as? Book, let book2 = object2 as? Book else { return .OrderedSame }
                 return book1.title.compare(book2.title)
             }
-                
-            return YapDatabaseView(grouping: grouping, sorting: sorting, versionTag: "0")
-        
-//        default:
-//            fatalError("Unknown group for events database view: \(group)")
+            
+            // This logic will only be run for updates to the books collection
+            let options = YapDatabaseViewOptions()
+            options.allowedCollections = YapWhitelistBlacklist(whitelist: ["books"] as Set<NSObject>)
+            
+            // Create the view
+            return YapDatabaseView(grouping: grouping, sorting: sorting, versionTag: "0", options: options)
         }
-        
     }
     
     var name: String {
@@ -50,6 +55,6 @@ func initializeDatabaseAtPath(databasePath: String) -> YapDatabase {
 
 extension YapDatabase {
     func registerCustomExtension(customDatabaseExtension: CustomDatabaseExtension) -> Bool {
-        return registerExtension(customDatabaseExtension.ext, withName: customDatabaseExtension.name)
+        return registerExtension(customDatabaseExtension.sampleExt, withName: customDatabaseExtension.name)
     }
 }
